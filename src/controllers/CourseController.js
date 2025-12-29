437011113558c545ca65ff6e1f9fd2b3b852cb35
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-29 13:17:54 -0300

    Trabalhando com operadores

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize')
+
 const Controller = require('./Controller.js')
 const CourseService = require('../services/CourseService.js')
 
@@ -6,6 +8,15 @@ const personService = new CourseService()
 class CourseController extends Controller {
   constructor () {
     super(personService)
+  }
+
+  async getCourses(req, res) {
+    const { start_date, end_date } = req.query
+    const where = {}
+
+    start_date || end_date ? where.start_date = {} : null
+    start_date ? where.start_date[Op.gte] = start_date : null
+    end_date ? where.start_date[Op.lte] = end_date : null
   }
 }
 
